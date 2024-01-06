d2c4b1b62e0b3cd8e28850674133ca114639c3da
Bruna Feyh <bruna.feyh@unioeste.br>
2024-01-06 13:22:16 -0300

    Add files via upload

---
+++ Soma.java
@@ -0,0 +1,10 @@
+
+public class Soma {
+    public static void main(String[] args) {
+        // Operação de soma
+        int resultado = 10 + 5;
+
+        // Imprimir o resultado sem aspas
+        System.out.println(resultado);
+    }
+}
